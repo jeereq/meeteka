9b479eb376f50bda80ccdee0ed27d233c96e9cbf
jeereq <mingandajeereq@gmail.com>
2024-11-27 20:37:46 +0100

    feat: save chnag e

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 import en from '../dictionnaries/en';
 import fr from '../dictionnaries/fr';
 type Language = 'en' | 'fr';
