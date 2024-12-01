cfe4ed81a842936ff20eedcae4778ab6be073a2c
jeereq <mingandajeereq@gmail.com>
2024-12-01 03:49:53 +0100

    feat: save change in pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  useEffect(function () {
+    navigate(pathname)
+  }, [pathname])
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
